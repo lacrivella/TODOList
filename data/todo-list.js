7cb5cca5e1f5ff147e7ae6f5cd523003302c9375
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-20 16:45:51 -0700

    completed TODO List Data and added utility helpers

---
+++ data/todo-list.js
@@ -0,0 +1,32 @@
+const todos = [
+    {
+        task: 'Catch up on Podcasts',
+        completed: true
+    },
+    {
+        task: 'Volunteer at Art Galleries',
+        completed: false
+    },
+    {
+        task: 'Keep It to Game of Thrones',
+        completed: true
+    },
+    {
+        task: 'Complain to Noah about Eurovision',
+        completed: false
+    },
+    {
+        task: 'Win Eurovision',
+        completed: true
+    },
+    {
+        task: 'Sleep Forever',
+        completed: false
+    },
+    {
+        task: 'Pet all the Dogs',
+        completed: true
+    }
+];
+
+export default todos;
