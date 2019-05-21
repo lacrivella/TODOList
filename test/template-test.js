a5e408c8c91a63057462890d6cb3723cef401add
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-20 18:54:18 -0700

    tdd'ed for false/unchecked

--- test/template-test.js
+++ test/template-test.js
@@ -3,7 +3,7 @@ const test = QUnit.test;
 QUnit.module('templating');
 
 function template(lists) {
-    let checked = '';
+    let checked ='';
     
     if(lists.completed) {
         checked = 'checked';
@@ -11,7 +11,7 @@ function template(lists) {
 
     return /*html*/`
     <div>
-        <input type="checkbox" ${checked}>
+        <input type="checkbox"${checked}>
         <label for="${lists.label}">${lists.task}</label>
     <div>
     `;
@@ -29,7 +29,30 @@ test('template returning checked', function(assert) {
 
     const expected = /*html*/`
         <div>
-            <input type="checkbox" checked>
+            <input type="checkbox"checked>
+            <label for="podcast">Catch up on Podcasts</label>
+        <div>
+    `;
+
+    const html = template(lists);
+    //Assert
+    assert.htmlEqual(html, expected);
+});
+
+test('template returning not checked', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const lists = {
+        task: 'Catch up on Podcasts',
+        completed: false,
+        label: 'podcast'
+    };
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+    const expected = /*html*/`
+        <div>
+            <input type="checkbox">
             <label for="podcast">Catch up on Podcasts</label>
         <div>
     `;
